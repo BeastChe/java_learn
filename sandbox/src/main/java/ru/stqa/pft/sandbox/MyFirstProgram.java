5ee3f5d6bbfc5e86147449faf9d6c164ad313e30
andrey.chechetkin <andreypodolskcity@gmail.com>
2020-03-25 19:03:37 +0300

    много чего, добавил тесты и селениум

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,7 +3,18 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
 	public static void main(String[] args) {
-		System.out.println("hi");
+		hello("world");
+		hello("Kot");
+
+		Square s = new Square(3);
+		System.out.println(s.area());
+
+		Prugl p = new Prugl(3,2);
+		System.out.println(p.area());
+	}
+
+	public static void hello(String somebody) {
+		System.out.println("Hello " + somebody + "!");
 	}
 
 }
